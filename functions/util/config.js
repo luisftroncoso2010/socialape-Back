d2a7e361278b06128cd57ae18d32fbb1fdee5db6
luisftroncoso2010 <troncober@hotmail.com>
2024-04-13 10:25:39 -0500

    Refactoring & Organizing

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyCKvNyu4eZo_tn6d_U231y1XFwr0YPd41s",
+    authDomain: "socialape-b5735.firebaseapp.com",
+    databaseURL: "https://socialape-b5735-default-rtdb.firebaseio.com",
+    projectId: "socialape-b5735",
+    storageBucket: "socialape-b5735.appspot.com",
+    messagingSenderId: "675924544926",
+    appId: "1:675924544926:web:e2ce9e39865d5a1ed8da8f",
+    measurementId: "G-X99WK3C2YV"
+  };
