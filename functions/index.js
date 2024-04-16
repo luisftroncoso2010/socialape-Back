a6587cb7445b03bf1c15b5607afb43b189c2759d
luisftroncoso2010 <troncober@hotmail.com>
2024-04-15 23:02:35 -0500

    Getting and Commenting on Post

--- functions/index.js
+++ functions/index.js
@@ -5,19 +5,27 @@ const app = express();
 
 const FBAuth = require("./util/fbAuth")
 
-const { getAllScreams, postOneScream} = require("./handlers/screams");
+const { getAllScreams, postOneScream, getScream, commentOnScream} = require("./handlers/screams");
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 // Scream routes
-app.get("/screams", getAllScreams);
-app.post("/scream", FBAuth, postOneScream);
-app.post("/user/image",FBAuth,  uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+//TODO: delete scream
+
+//TODO: like a scream
+
+//TODO: un like a scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+
 
 // Users route. Registro de usuarios  
-app.post("/signup", signup);
-app.post("/login", login);
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image',FBAuth,  uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
