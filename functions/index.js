d197e5697d1db2488b021da23dd93df01e75dacb
luisftroncoso2010 <troncober@hotmail.com>
2024-04-17 16:29:12 -0500

    Like, Unlike and Delete Post

--- functions/index.js
+++ functions/index.js
@@ -5,18 +5,16 @@ const app = express();
 
 const FBAuth = require("./util/fbAuth")
 
-const { getAllScreams, postOneScream, getScream, commentOnScream} = require("./handlers/screams");
+const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream} = require("./handlers/screams");
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-//TODO: delete scream
-
-//TODO: like a scream
-
-//TODO: un like a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 
